635653536be2c998c044b841d50d0256d1a33ea1
Lionel Beato <lionel.beato@gmail.com>
2020-11-15 20:04:47 -0500

    working endpoints NOTE: doesn't update directly in browser, stream will close on that end

--- src/main/java/works/lionel/reactive/controller/MessageController.java
+++ src/main/java/works/lionel/reactive/controller/MessageController.java
@@ -3,6 +3,7 @@ package works.lionel.reactive.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -10,10 +11,12 @@ import works.lionel.reactive.model.Message;
 import works.lionel.reactive.repository.MessageRepository;
 
 import java.time.Duration;
+import java.time.LocalTime;
 import java.util.Random;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+@CrossOrigin
 @RestController
 public class MessageController {
 
@@ -38,10 +41,9 @@ public class MessageController {
     }
 
 //    @GetMapping(path="/all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "/all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     private Flux<Message> getAllMessages() {
-        return messageRepository.findAll()
-                .delayElements(Duration.ofSeconds(1));
+        return messageRepository.findAll().delayElements(Duration.ofMillis(500));
     }
 
     @GetMapping(value = "test",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -55,9 +57,12 @@ public class MessageController {
 
     @GetMapping("/what")
     private Mono<Message> getWhat() {
-//        return Mono.just("what?");
         return Mono.just(new Message("2", "yup"));
     }
 
-
+    @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> "Flux - " + LocalTime.now().toString());
+    }
 }
--- src/main/java/works/lionel/reactive/repository/MessageRepository.java
+++ src/main/java/works/lionel/reactive/repository/MessageRepository.java
@@ -1,14 +1,20 @@
 package works.lionel.reactive.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.data.repository.reactive.RxJava2CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import works.lionel.reactive.model.Message;
 import java.util.UUID;
 
 //@Repository
 public interface MessageRepository extends ReactiveMongoRepository<Message, String> {
+
+    @Tailable
+    Flux<Message> findWithTailableCursorBy();
+
 }
